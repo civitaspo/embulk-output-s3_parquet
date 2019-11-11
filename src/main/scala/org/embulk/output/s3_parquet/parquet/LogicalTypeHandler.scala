d77d63b41a497c365728d18d40e5581e02ff70b7
Ryo Okubo <syucream1031@gmail.com>
2019-11-12 01:42:04 +0900

    Use DataException on processing actual datum

--- src/main/scala/org/embulk/output/s3_parquet/parquet/LogicalTypeHandler.scala
+++ src/main/scala/org/embulk/output/s3_parquet/parquet/LogicalTypeHandler.scala
@@ -3,6 +3,7 @@ package org.embulk.output.s3_parquet.parquet
 import org.apache.parquet.io.api.{Binary, RecordConsumer}
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName
 import org.apache.parquet.schema.{OriginalType, PrimitiveType, Type}
+import org.embulk.spi.DataException
 import org.embulk.spi.time.Timestamp
 import org.msgpack.value.Value
 
@@ -27,7 +28,7 @@ case class TimestampMillisLogicalTypeHandler() extends LogicalTypeHandler {
     override def consume(orig: AnyRef, recordConsumer: RecordConsumer): Unit =
         orig match {
             case ts: Timestamp => recordConsumer.addLong(ts.toEpochMilli)
-            case _ => throw new IllegalArgumentException("given mismatched type value")
+            case _ => throw new DataException("given mismatched type value")
         }
 }
 
@@ -40,7 +41,7 @@ case class TimestampMicrosLogicalTypeHandler() extends LogicalTypeHandler {
             case ts: Timestamp =>
                 val v = (ts.getEpochSecond * 1_000_000L) + (ts.getNano.asInstanceOf[Long] / 1_000L)
                 recordConsumer.addLong(v)
-            case _ => throw new IllegalArgumentException("given mismatched type value")
+            case _ => throw new DataException("given mismatched type value")
         }
 }
 
@@ -53,6 +54,6 @@ case class JsonLogicalTypeHandler() extends LogicalTypeHandler {
             case msgPack: Value =>
                 val bin = Binary.fromString(msgPack.toJson)
                 recordConsumer.addBinary(bin)
-            case _ => throw new IllegalArgumentException("given mismatched type value")
+            case _ => throw new DataException("given mismatched type value")
         }
 }
